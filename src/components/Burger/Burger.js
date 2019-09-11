2c51c38964d25f8edd2d18682c36a5a28dfd0ec7
Chanaka Rathnayaka <bmcrathnayaka@gmail.com>
2019-09-11 23:33:17 +0530

    added dynamic behaviour for ingredient rendering

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,17 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey=>{
+            return [...Array(props.ingredients[igKey])].map((_,i)=>{
+                return <BurgerIngredient type={igKey} key={igKey+i}></BurgerIngredient>
+            })
+        });
+    
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"></BurgerIngredient>
-            <BurgerIngredient type="cheese"></BurgerIngredient>
-            <BurgerIngredient type="meat"></BurgerIngredient>
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom"></BurgerIngredient>
         </div>
     );
