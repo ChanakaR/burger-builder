0906e5ea753cafba917902c8c8193767ab4b02e0
Chanaka Rathnayaka <bmcrathnayaka@gmail.com>
2019-09-25 19:21:26 +0530

    added order summary functionality

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import Aux from '../../../hoc/Aux';
+
+const orderSummary = (props) => {
+    const ingredientSummary = Object.keys(props.ingredients)
+        .map(igKey => {
+            return <li><span style={{textTransform:"capitalize"}}>{igKey}</span>: {props.ingredients[igKey]}</li>
+        });
+    
+    return (
+        <Aux>
+            <h3>Your Order</h3>
+            <p>A delicious Burger with following Ingredients: </p>
+            <ul>
+                {ingredientSummary}
+            </ul>
+        </Aux>
+    );
+}
+
+export default orderSummary;
