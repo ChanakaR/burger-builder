29cca9ccf5ca24713c900a245e4ed8d05573dd0d
Chanaka Rathnayaka <bmcrathnayaka@gmail.com>
2019-10-19 21:45:08 +0530

    added toolbar component

---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const toolbar = (props) => {
+    return (
+        <header>
+            <div>MENU</div>
+            <div>LOGO</div>
+            <nav>
+                ...
+            </nav>
+        </header>
+    );
+}
+
+export default toolbar;
