17d099cd3b54abc619abb5b67f6071a5f4aaafdb
Chanaka Rathnayaka <bmcrathnayaka@gmail.com>
2019-10-20 19:00:37 +0530

    stop update the order summary unnecessarily

--- src/components/UI/modal/Modal.js
+++ src/components/UI/modal/Modal.js
@@ -1,21 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
 import classes from './Modal.css';
 import Aux from '../../../hoc/Aux';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = (props) => (
-    <Aux>
-        <Backdrop show={props.show} clicks={props.modalClosed}></Backdrop>
-        <div 
-            className={classes.Modal}
-            style={{
-                transform: props.show ? 'translateY(0)': 'translateY(-100vh)',
-                opacity: props.show ? 1 : 0
-            }}
-            >
-            {props.children}
-        </div>
-    </Aux>
-);
+class Modal extends Component {
 
-export default modal;
+    shouldComponentUpdate (nextProps, nextState) {
+        if(nextProps.show !== this.props.show){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    render () {
+        return (
+            <Aux>
+                <Backdrop show={this.props.show} clicks={this.props.modalClosed}></Backdrop>
+                <div 
+                    className={classes.Modal}
+                    style={{
+                        transform: this.props.show ? 'translateY(0)': 'translateY(-100vh)',
+                        opacity: this.props.show ? 1 : 0
+                    }}
+                    >
+                    {this.props.children}
+                </div>
+            </Aux>
+        );
+    }
+}
+
+export default Modal;
